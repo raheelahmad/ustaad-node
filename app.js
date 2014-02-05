87070c144040b46c8f0f1afbf6dcd754cb5d7e67
Raheel Ahmad <ahmadraheel@gmail.com>
2014-02-04 23:54:49 -0800

    Adding route handlers for GET & POST cards

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 var http = require('http');
 
+function showCards(req, res) {
+  res.end('Here all the cards!');
+}
+
+function addCard(req, res) {
+  res.end('Adding to cards!');
+}
+
+function notFound(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 404;
+  message = JSON.stringify({'message': 'Cannot process that request.'});
+  res.end(message);
+}
+
 var server = http.createServer( function(req, res) {
   console.log(req.url);
-  res.end('Done');
+  if (req.url === '/cards' && req.method == 'GET') {
+    showCards(req, res);
+  } else if (req.url === '/cards' && req.method == 'POST') {
+    addCard(req, res);
+  } else {
+    notFound(req, res);
+  }
 });
 
 port = process.env.PORT_NUM || 3100;
