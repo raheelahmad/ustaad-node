c8871b53651241fac5e417826f69b468996fbce7
Raheel Ahmad <ahmadraheel@gmail.com>
2014-02-05 20:31:37 -0800

    Connect to mongoose

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 var http = require('http');
+var mongoose = require('mongoose');
+
+var routes = require('./routes');
 var cards = require('./routes/cards');
 
-function notFound(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 404;
-  message = JSON.stringify({'message': 'Cannot process that request.'});
-  res.end(message);
+function setupMongoose() {
+  var uriString = process.env.MONGOLAB_URI ||
+                  process.env.MONGOHQ_URI ||
+                  'mongodb://localhost/ustaad';
+  mongoose.connect(uriString, function(err, res) {
+    if (err) { console.log('ERROR connecting to ' + uriString + 
+                          '. ' + err);
+    } else {
+      console.log('Connected to mongoose at ' + uriString);
+    }
+  });
 }
 
 var server = http.createServer( function(req, res) {
   console.log(req.url);
-  console.log(req.headers);
   if (req.url === '/cards' && req.method == 'GET') {
     cards.showCards(req, res);
   } else if (req.url === '/cards' && req.method == 'POST') {
     cards.addCard(req, res);
   } else {
-    notFound(req, res);
+    routes.notFound(req, res);
   }
 });
 
 var port = Number(process.env.PORT || 3100);
 server.listen(port, function() {
   console.log('listening on ' + port);
+  setupMongoose();
 });
 
